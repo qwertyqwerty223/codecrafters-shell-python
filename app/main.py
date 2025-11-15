6c686e551e04177ff62d49ef63d3c90f5937c639
Taranjot Singh Pannu <pannutaranjot@hotmail.com>
2025-11-15 13:21:45 -0800

    implemented exit

--- app/main.py
+++ app/main.py
@@ -2,13 +2,25 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
+        exit_status_codes = ['0', '1']
         sys.stdout.write("$ ")
         user_input = input()
+
         if user_input:
+            cmd = user_input.split(' ')
+
+            # exit command
+            if cmd[0] == "exit":
+                if len(cmd) > 1 and cmd[1] in exit_status_codes:
+                    sys.exit(int(cmd[1]))
+                else:
+                    sys.exit()
+
+            # to handle rest of the commands (for now)
             error_msg = f"{user_input}: command not found"
             print(error_msg)
+        
 
 
 if __name__ == "__main__":
