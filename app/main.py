12af3b718651d83c1d40fd6211587713c168b781
Taranjot Singh Pannu <pannutaranjot@hotmail.com>
2025-11-15 13:43:17 -0800

    updated shell mechanism and implemented echo

--- app/main.py
+++ app/main.py
@@ -1,25 +1,45 @@
 import sys
 
+def invalid_cmd(cmd):
+    error_msg = f"{cmd}: command not found"
+    print(error_msg)
+
+
+def cmd_exit(status_code='0'):
+        exit_status_codes = ['0', '1']
+        if status_code in exit_status_codes:
+            sys.exit(int(status_code))
+        else:
+            invalid_cmd(f"exit {status_code}")
+
+
+def cmd_echo(*args):
+    print(*args)
+
+
+cmd_dict = {
+    "exit": cmd_exit,
+    "echo": cmd_echo
+}
+
 
 def main():
     while True:
-        exit_status_codes = ['0', '1']
         sys.stdout.write("$ ")
         user_input = input()
 
         if user_input:
             cmd = user_input.split(' ')
 
-            # exit command
-            if cmd[0] == "exit":
-                if len(cmd) > 1 and cmd[1] in exit_status_codes:
-                    sys.exit(int(cmd[1]))
+            # handle existing commands
+            if cmd[0] in cmd_dict:
+                if len(cmd) > 1:
+                    cmd_dict[cmd[0]](*(cmd[1:]))
                 else:
-                    sys.exit()
-
+                    cmd_dict[cmd[0]]()
+            else:
             # to handle rest of the commands (for now)
-            error_msg = f"{user_input}: command not found"
-            print(error_msg)
+                invalid_cmd(user_input)
         
 
 
